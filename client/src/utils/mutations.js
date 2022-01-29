5bfa441c59430420e981ed8e6da136df4dbdf7c9
anushaselvan <anushaselvan@gmail.com>
2022-01-29 11:13:37 +1100

    added queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors:[string], $description: string!, $bookId: String!, $image: String, $link: String) {
+    saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link) {
+      savedBooks{
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookIId: String!) {
+    removeBook(bookId: $bookId) {
+      savedBooks{
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
